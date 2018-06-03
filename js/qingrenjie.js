7dd6a4172212ff38bbdaede5735e1ae047487c9d
baiguanghui <724618014@qq.com>
2018-06-03 12:14:00 +0800

    333333

--- js/qingrenjie.js
+++ js/qingrenjie.js
@@ -1,5 +1,15 @@
 $(function(){
 
+    $(function(){
+       $(".box>label").click(function(){
+           $(this).addClass("on").siblings().removeClass("on");
+           if($(this).hasClass("last")){
+               $(this.children("input").focus())
+           }
+       })
+
+    })
+
 $(".hd").click(function () {
     alert("活动规则")
 })
